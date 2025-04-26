7e1b51576fdb7c3daf88ec45a3ba66a55ebdd147
AnqiHe13 <125236048+AnqiHe13@users.noreply.github.com>
2025-04-25 17:14:47 -0700

    Follow politeness delay for each site, in frontier.py and worker.py

--- crawler/frontier.py
+++ crawler/frontier.py
@@ -1,5 +1,6 @@
 import os
 import shelve
+import time
 
 from threading import Thread, RLock
 from queue import Queue, Empty
@@ -7,11 +8,21 @@ from queue import Queue, Empty
 from utils import get_logger, get_urlhash, normalize
 from scraper import is_valid
 
+#adding extra libs
+from collections import defaultdict
+from urllib.parse import urlparse, urldefrag
+
 class Frontier(object):
     def __init__(self, config, restart):
         self.logger = get_logger("FRONTIER")
         self.config = config
-        self.to_be_downloaded = list()
+        self.to_be_downloaded = []
+
+        ## adding more needed attributes
+        self.Lock = RLock()
+        self.domain_last_access = {}
+        self.subdomains = defaultdict(set)
+        self.unique_urls = set()
         
         if not os.path.exists(self.config.save_file) and not restart:
             # Save file does not exist, but request to load save.
@@ -35,6 +46,9 @@ class Frontier(object):
                 for url in self.config.seed_urls:
                     self.add_url(url)
 
+
+
+
     def _parse_save_file(self):
         ''' This function can be overridden for alternate saving techniques. '''
         total_count = len(self.save)
@@ -42,31 +56,116 @@ class Frontier(object):
         for url, completed in self.save.values():
             if not completed and is_valid(url):
                 self.to_be_downloaded.append(url)
+
+                #building the unique urls set
+                self.unique_urls.add(url)
+
                 tbd_count += 1
         self.logger.info(
             f"Found {tbd_count} urls to be downloaded from {total_count} "
             f"total urls discovered.")
 
     def get_tbd_url(self):
-        try:
-            return self.to_be_downloaded.pop()
-        except IndexError:
+
+        # make sure only one thread at a time for the thread-safe purpose
+        with self.Lock:
+            now = time.time()
+            for i, url in enumerate(self.to_be_downloaded):
+                domain = urlparse(url).netloc
+                last_access = self.domain_last_access.get(domain, 0)
+                crawl_delay = self.worker.config.get_crawl_delay(domain)
+
+                # Respect the crawl delay
+                if now - last_access >= crawl_delay:
+                    self.domain_last_access[domain] = now
+                    return self.to_be_downloaded.pop(i)
             return None
 
     def add_url(self, url):
-        url = normalize(url)
-        urlhash = get_urlhash(url)
-        if urlhash not in self.save:
-            self.save[urlhash] = (url, False)
-            self.save.sync()
-            self.to_be_downloaded.append(url)
-    
-    def mark_url_complete(self, url):
-        urlhash = get_urlhash(url)
-        if urlhash not in self.save:
-            # This should not happen.
-            self.logger.error(
-                f"Completed url {url}, but have not seen it before.")
 
-        self.save[urlhash] = (url, True)
-        self.save.sync()
+        #make sure only one thread at a time for the thread-safe purpose
+        with self.Lock:
+            url = normalize(url)
+            #get rid of the fragment and starting here using fragment_clean version urls
+            unfrag_url, _ = urldefrag(url)
+
+            urlhash = get_urlhash(unfrag_url)
+            if urlhash not in self.save:
+                self.save[urlhash] = (unfrag_url, False)
+                self.save.sync()
+
+                self.to_be_downloaded.append(unfrag_url)
+
+                #building up the unique URLs set
+                self.unique_urls.add(unfrag_url)
+                #checking if the url is missing and also is allowed to be crawled
+                if self.check_subdomain(unfrag_url):
+                    self.subdomains[unfrag_url.hostname].add(unfrag_url)
+
+    def mark_url_complete(self, url):
+
+        # make sure only one thread at a time for the thread-safe purpose
+        with self.Lock:
+            urlhash = get_urlhash(url)
+            if urlhash not in self.save:
+                # This should not happen.
+                self.logger.error(
+                    f"Completed url {url}, but have not seen it before.")
+
+            self.save[urlhash] = (url, True)
+            self.save.sync()
+
+
+
+    # checking if the url is following the instruction of
+    #*.ics.uci.edu/*
+    #*.cs.uci.edu/*
+    #*.informatics.uci.edu/*
+    #*.stat.uci.edu/*
+    #today.uci.edu/department/information_computer_sciences/*
+    def check_subdomain(self, url):
+        try:
+            parsed_url = urlparse(url)
+            hostname = parsed_url.hostname
+            path = parsed_url.path
+
+            if hostname is None:
+                return False
+
+            assigned_domains = (
+                ".ics.uci.edu",
+                ".cs.uci.edu",
+                ".informatics.uci.edu",
+                ".stat.uci.edu"
+            )
+
+            if any(hostname.endswith(domain) for domain in assigned_domains):
+                return True
+
+            if hostname == ("today.uci.edu") and path.startswith("/department/information_computer_sciences"):
+                return True
+
+            return False
+
+        except Exception as e:
+            self.logger.error(f"Error on {url}: {e}")
+            return False
+
+
+
+
+    # 1.How many unique pages did you find?
+    # Uniqueness for the purposes of this assignment is ONLY established by the URL,
+    # but discarding the fragment part.
+    def print_unique_urls(self):
+        print(f"There are total of {len(self.unique_urls)} unique pages")
+
+
+
+    # 4. How many subdomains did you find in the uci.edu domain?
+    def print_subdomains(self):
+        print("The following are the subdomains:")
+        for subdomain in sorted(self.subdomains):
+            print(f"{subdomain}", len(self.subdomains[subdomain]))
+
+
--- crawler/worker.py
+++ crawler/worker.py
@@ -1,34 +1,174 @@
 from threading import Thread
 
 from inspect import getsource
+from urllib.parse import urlparse
 from utils.download import download
 from utils import get_logger
 import scraper
 import time
+import threading
+from robotexclusionrulesparser import RobotFileParser
 
 
 class Worker(Thread):
     def __init__(self, worker_id, config, frontier):
+
         self.logger = get_logger(f"Worker-{worker_id}", "Worker")
         self.config = config
         self.frontier = frontier
+        self.robot_parsers = {} # store robots.txt parsers for each domain
+        
         # basic check for requests in scraper
         assert {getsource(scraper).find(req) for req in {"from requests import", "import requests"}} == {-1}, "Do not use requests in scraper.py"
         assert {getsource(scraper).find(req) for req in {"from urllib.request import", "import urllib.request"}} == {-1}, "Do not use urllib.request in scraper.py"
         super().__init__(daemon=True)
+
+
+    def get_crawl_delay(self, domain):
+        """
+        Get the crawl delay for the domain from robots.txt.
+        """
+        rp = self.robot_parsers.get(domain)
+        if rp is None:
+            return self.config.time_delay
+        
+        # Get Crawl-Delay
+        crawl_delay = rp.get_crawl_delay("*")
+        if crawl_delay is None:
+            return self.config.time_delay
+        return crawl_delay
+
+
+    def can_fetch(self, url):
+        """
+        Check if the URL can be fetched based on robots.txt rules.
+        """
+        parsed_url = urlparse(url)
+        domain = f"{parsed_url.scheme}://{parsed_url.netloc}"
+        
+        if domain not in self.robot_parsers:
+            # Download and parse robots.txt
+            robots_url = f"{domain}/robots.txt"
+            self.logger.info(f"Fetching robots.txt from {robots_url}")
+            rp = RobotFileParser()
+            try:
+                rp.fetch(robots_url)
+                self.robot_parsers[domain] = rp
+            except Exception as e:
+                self.logger.warning(f"Failed to fetch robots.txt from {robots_url}: {e}")
+                self.robot_parsers[domain] = None
+        
+        rp = self.robot_parsers.get(domain)
+        if rp is None:
+            return True
+        
+        # To check if the URL can be fetched
+        return rp.is_allowed("*", url)
+
         
     def run(self):
+        retry_delay = 5  # todo: make this configurable
+        max_file_size = 10 * 1024 * 1024  # 10 MB, todo: make this configurable
+        min_file_size = 100  # todo: make this configurable
         while True:
             tbd_url = self.frontier.get_tbd_url()
+
+            # sleep and retry
             if not tbd_url:
-                self.logger.info("Frontier is empty. Stopping Crawler.")
-                break
+                # sleep for a while before checking again
+                self.logger.info(f"Frontier is empty. Sleeping for {retry_delay}s.")
+                time.sleep(retry_delay)
+                tbd_url = self.frontier.get_tbd_url()
+                if not tbd_url:
+                    # if still empty, stop the crawler
+                    self.frontier.stop()
+                    self.frontier.save()
+                    self.logger.info("Frontier is empty. Stopping Crawler.")
+                    break
+                else:
+                    continue
+
+            # check if the URL can be fetched based on robots.txt rules
+            if not self.can_fetch(tbd_url):
+                self.logger.info(f"Skipping {tbd_url} due to robots.txt rules.")
+                self.frontier.mark_url_complete(tbd_url)
+                continue
+
+            # get crawl delay for the domain
+            parsed_url = urlparse(tbd_url)
+            domain = f"{parsed_url.scheme}://{parsed_url.netloc}"
+            crawl_delay = self.get_crawl_delay(domain)
+
+            # download the URL    
             resp = download(tbd_url, self.config, self.logger)
+
+            # Check if the response is valid
+            if resp is None:
+                self.logger.warning(f"Failed to download {tbd_url}. Skipping.")
+                self.frontier.mark_url_complete(tbd_url)
+                continue
+
+            # Check if the content length is too large
+            content_length = resp.raw_response.headers.get("Content-Length")
+            if content_length and int(content_length) > max_file_size:
+                self.logger.info(f"Skipping {tbd_url} due to large file size ({content_length} bytes).")
+                self.frontier.mark_url_complete(tbd_url)
+                continue
+
+            # Check if the response content is too small
+            if len(resp.raw_response.content) < min_file_size:
+                self.logger.info(f"Skipping {tbd_url} because content is too small ({len(resp.raw_response.content)} bytes).")
+                self.frontier.mark_url_complete(tbd_url)
+                continue
+
             self.logger.info(
                 f"Downloaded {tbd_url}, status <{resp.status}>, "
                 f"using cache {self.config.cache_server}.")
+            
             scraped_urls = scraper.scraper(tbd_url, resp)
             for scraped_url in scraped_urls:
                 self.frontier.add_url(scraped_url)
             self.frontier.mark_url_complete(tbd_url)
-            time.sleep(self.config.time_delay)
+
+            # sleep for the crawl delay before the next request
+            self.logger.info(f"Sleeping for {crawl_delay}s before next request.")
+            time.sleep(crawl_delay)
+
+
+def start_workers(config, frontier):
+    """
+    Start a worker thread.
+    """
+    logger = get_logger(f"Worker-{worker_id}", "Worker")
+    workers = []
+
+    # start all the worker threads
+    for worker_id in range(config.threads_count):
+        worker = Worker(worker_id, config, frontier)
+        workers.append(worker)
+        worker.start()
+        logger.info(f"Worker-{worker_id} started.")
+
+    # print status of all the workers
+    def print_status_loop():
+        interval = 120 # seconds # todo: make this configurable
+        while True:
+            alive = sum(1 for w in workers if w.is_alive())
+            qsize = frontier.queue_size()
+            logger.info(
+                f"Status: active_workers={alive}, queue_size={qsize}"
+            )
+            if alive == 0:
+                break
+            time.sleep(interval)
+
+    status_thread = threading.Thread(
+        target=print_status_loop, name="StatusThread", daemon=True
+    )
+    status_thread.start()
+
+    # wait for all the workers to finish
+    for w in workers:
+        w.join()
+    status_thread.join()
+    logger.info("All workers finished")
